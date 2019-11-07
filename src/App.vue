6879b07f6319dfd63dd3803c9fa7f0241dc95446
micasrur <50995869+micasrur@users.noreply.github.com>
2019-11-07 20:51:21 -0300

    Add files via upload

---
+++ src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <div id="app">
+        <Inicio msg="PIEDRA PAPEL O TIJERA"/>
+    <button type="button" class="btn btn-dark">Comenzar juego!</button>
+   
+  </div>
+</template>
+
+<script>
+import Inicio from './components/Inicio.vue'
+
+export default {
+  name: 'app',
+  components: {
+    Inicio
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
